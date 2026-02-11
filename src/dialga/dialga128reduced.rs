fb9c4be5b5f10f01568c1541cceb7c15cf302c63
t-ESC <theo.krinitz@proton.me>
2026-02-11 22:21:02 +0100

    WIP: Implement r_m

--- src/dialga/dialga128reduced.rs
+++ src/dialga/dialga128reduced.rs
@@ -1,14 +1,16 @@
+use crate::dialga::ms::ms;
 use crate::dialga::roundfunction::r_i::*;
 use crate::dialga::helper::state::{State};
-use crate::dialga::roundconstants::*;
+use crate::dialga::roundconstants::{self, *};
+use crate::dialga::roundfunction::sub_cell::sub_cell;
 
 const ALPHA:usize = 5;
 
 pub fn encrypt(plaintext: u128, tweak: u128, key: [u128; 2]) -> u128 {
     let mut state_d = State::from(plaintext ^ key[0] ^ key[1] ^ tweak);
-    let state_t = State::from(tweak);
+    let mut state_t = State::from(tweak);
 
-    r_f(&mut state_d, &state_t, key);
+    r_f(&mut state_d, &mut state_t, key);
     // r_m
     // r_b
     let ciphertext: u128 = state_d.into();
@@ -17,15 +19,15 @@ pub fn encrypt(plaintext: u128, tweak: u128, key: [u128; 2]) -> u128 {
 
 pub fn decrypt(ciphertext: u128, tweak: u128, key: [u128; 2]) -> u128 {
     let mut state_d = State::from(ciphertext);
-    let state_t = State::from(tweak);
+    let mut state_t = State::from(tweak);
 
-    r_f_inv(&mut state_d, &state_t, key);
+    r_f_inv(&mut state_d, &mut state_t, key);
 
     let plaintext:u128 = state_d.into();
     plaintext ^ key[0] ^ key[1] ^ tweak
 }
 
-fn r_f(state_d: &mut State, tweak: &State, key: [u128; 2]) {
+fn r_f(state_d: &mut State, tweak: &mut State, key: [u128; 2]) {
     // Tweak Schedule
     let mut t_r: [State; ALPHA] = [State::from(0); ALPHA]; // Index will be -1 from round since round starts at 1 (whyyyyyy)
     for i in 1..=ALPHA {
@@ -36,6 +38,7 @@ fn r_f(state_d: &mut State, tweak: &State, key: [u128; 2]) {
             t_r[i-1] = t_i ^ key[(i-1)%2];
         }
     }
+    *tweak = t_r[ALPHA-1];
 
     for i in 1..=ALPHA {
         if i == 1 {
@@ -56,7 +59,22 @@ fn r_f(state_d: &mut State, tweak: &State, key: [u128; 2]) {
     }
 }
 
-fn r_f_inv(state_d: &mut State, tweak: &State, key: [u128; 2]) {
+fn r_m(state_d: &mut State, tweak: &mut State, key: [u128; 2]) {
+    // Tweak Schedule
+    let mut t_r: [State; 2] = [State::from(0); 2];
+    t_r[0] = sub_cell(tweak);
+    *tweak = *tweak ^ State::from(key[(ALPHA-1) % 2]);
+    t_r[1] = r_i_inv(tweak, 1);
+
+    //Data Schedule
+    r_i(state_d, (2*ALPHA)%4);
+    *state_d ^=  t_r[0] ^ key[(ALPHA - 1)%2] ^ C_M[0];
+
+    r_i(state_d, (2*ALPHA + 1)%4);
+    *state_d ^= ms(&mut t_r[1]) ^ C_M[1];
+}
+
+fn r_f_inv(state_d: &mut State, tweak: &mut State, key: [u128; 2]) {
     // Tweak Schedule
     let mut t_r: [State; ALPHA] = [State::from(0); ALPHA]; // Index will be -1 from round since round starts at 1 (whyyyyyy)
     for i in 1..=ALPHA {
