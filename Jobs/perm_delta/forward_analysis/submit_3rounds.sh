cf020d75efb89c7ebe81865bd87db65cac15363f
t-ESC <theo.krinitz@proton.me>
2026-06-02 08:34:55 +0200

    Job array fix

--- Jobs/perm_delta/forward_analysis/submit_3rounds.sh
+++ Jobs/perm_delta/forward_analysis/submit_3rounds.sh
@@ -3,11 +3,11 @@
 ### -- specify queue -- 
 #BSUB -q hpc
 ### -- se3-round-cryptanalysis 
-#BSUB -J round_cryptanalysis_f3[0, 20, 40, 60, 80, 100, 120, 140, 160, 180, 200, 220, 240, 260, 280, 300, 320, 340, 360, 380, 400, 420, 440, 460]%4
+#BSUB -J round_cryptanalysis_f3[0,20,40,60,80,100,120,140,160,180,200,220,240,260,280,300,320,340,360,380,400,420,440,460]%4
 ### -- as1 
 #BSUB -n 16
 #BSUB -R "rusage[mem=512MB]"
-#BSUB -W 24:00 
+#BSUB -W 6:00 
 ### -- specify that the cores must be on the same host -- 
 #BSUB -o Output_%J.out
 #BSUB -e Output_%J.err
